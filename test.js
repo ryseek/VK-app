544d9e1062a68f0f65b238c8d554010d8574eaa9
Ryseek <ryseek1@gmail.com>
2017-01-21 17:45:06 +0300

    asd

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ VK.api("users.get",{"user_ids":"","fields":"","name_case":"nom"},function(data) 
     if (data.response) {
         var responsevk=data.response[0];
 
-        fname=data.response[0].first_name;
+        fname=responsevk["first_name"];
 
         document.getElementById("firstname").innerHTML=fname;
     }
