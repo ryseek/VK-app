3eac0075b2b169bfe56720750828fd2895f9885c
Ryseek <ryseek1@gmail.com>
2017-01-21 14:05:58 +0300

    pzl work

--- vk_height.js
+++ vk_height.js
@@ -1,9 +1,9 @@
 var uid, fname, surname;
-var responsevk;
+
 
 VK.api("users.get",{"user_ids":"","fields":"","name_case":""},function(data) {
     if (data.response) {
-        responsevk=data.response[0];
+        var responsevk=data.response[0];
         uid=responsevk.id;
         fname=responsevk.first_name;
         surname=responsevk.sur_name;
