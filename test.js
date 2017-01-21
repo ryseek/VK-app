81430db71d64cc3047387a777d68f599167880cf
Ryseek <ryseek1@gmail.com>
2017-01-21 17:29:44 +0300

    testjs

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-document.getElementById("mr").innerHTML="asdasd";
+
+
+var uid, fname, surname;
+
+VK.api("users.get",{"user_ids":"","fields":"","name_case":"nom"},function(data) {
+    if (data.response) {
+        var responsevk=data.response[0];
+
+        fname=responsevk["first_name"];
+
+        document.getElementById("firstname").innerHTML=fname;
+    }
+});
+
+
+
+
+
+
