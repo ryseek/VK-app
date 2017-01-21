aaa80e0a7a115030e71a350203120aa36370dfdd
Ryseek <ryseek1@gmail.com>
2017-01-21 13:52:22 +0300

    yo

--- vk_height.js
+++ vk_height.js
@@ -1,15 +1,8 @@
 
-VK.init(function() {
-    // API initialization succeeded
-    // Your code here
-}, function() {
-    // API initialization failed
-    // Can reload page here
-}, '5.62');
-
 
 var id, fname, surname;
 var response;
+
 VK.api("users.get",{"user_ids":"","fields":"","name_case":""},function(data) {
 
     if (data.response) {
