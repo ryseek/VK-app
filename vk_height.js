32983eec06d5202fb4d8efa9ec7956544f09a791
Ryseek <ryseek1@gmail.com>
2017-01-21 13:46:38 +0300

    asdasd

--- vk_height.js
+++ vk_height.js
@@ -1,10 +1,11 @@
 
-
-
 VK.init(function() {
-
-
-} );
+    // API initialization succeeded
+    // Your code here
+}, function() {
+    // API initialization failed
+    // Can reload page here
+}, '5.62');
 
 
 var id, fname, surname;
@@ -13,7 +14,7 @@ VK.api("users.get",{"user_ids":"","fields":"","name_case":""},function(data) {
 
     if (data.response) {
         response=data.response[0];
-        id=response.uid;
+        id=response.id;
         fname=response.first_name;
         surname=response.sur_name;
 
@@ -21,3 +22,9 @@ VK.api("users.get",{"user_ids":"","fields":"","name_case":""},function(data) {
 
     }
 });
+
+
+
+
+
+
