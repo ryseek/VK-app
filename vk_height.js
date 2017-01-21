3cadb097e2cfbd3f9c9d060bbabfc9d0759391ca
Ryseek <ryseek1@gmail.com>
2017-01-21 15:16:54 +0300

    dafuck

--- vk_height.js
+++ vk_height.js
@@ -6,7 +6,7 @@ VK.api("users.get",{"user_ids":"","fields":"","name_case":""},function(data) {
     if (data.response) {
         var responsevk=data.response[0];
         console.log("data:"+JSON.parse(responsevk));
-
+        console.log("dafuck");
         fname=responsevk.first_name;
         surname=responsevk.sur_name;
 
