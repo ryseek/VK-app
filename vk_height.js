a3ccc62b7755ef114a4a3c37a0781162c33d6101
Ryseek <ryseek1@gmail.com>
2017-01-21 14:59:51 +0300

    562

--- vk_height.js
+++ vk_height.js
@@ -1,11 +1,11 @@
 
-document.getElementById("mr").innerHTML="MR work yea";
+document.getElementById("mr").innerHTML="MR 562";
 
 var uid, fname, surname;
 
 VK.api("users.get",{"user_ids":"","fields":"","name_case":""},function(data) {
     if (data.response) {
-        var responsevk=JSON.parse(data.response[0]);
+        var responsevk=data.response[0];
         fname=responsevk.first_name;
         surname=responsevk.sur_name;
 
