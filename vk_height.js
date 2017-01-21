616d22894679d099d0488e847c88b6014370fa8f
Ryseek <ryseek1@gmail.com>
2017-01-21 13:24:01 +0300

    pretty

--- vk_height.js
+++ vk_height.js
@@ -1,27 +1,22 @@
-VK.init(
-    function(){
-    });
 
-function autosize(width) {
-    //Проверяем элемент body на наличие.
-    if (!document.getElementById('body')) {
-        alert('error');
-        return;
+
+
+VK.init(function() {
+
+
+} );
+
+
+var id, name, surname;
+
+VK.api("users.get",{"user_ids":"","fields":"","name_case":""},function(data) {
+
+    if (data.response) {
+        var response=data.response[0];
+        id=response.uid;
+        document.getElementById("iname").innerHTML=id;
+
+
+
     }
-    // Успешно ли подключен ВК скрипт
-    if (typeof VK.callMethod != 'undefined') {
-        /*
-         Вызываем функцию vk js api для управления окном.
-         VK.callMethod('функция', параметры)
-         В данном случае у нас - VK.callMethod('изменение_размеров_окна', ширина, высота);
-         Так же добавляем еще 60 пикселей что бы было небольшое расстояние.
-         */
-        VK.callMethod('resizeWindow', 840, document.getElementById('body').clientHeight + 60);
-    } else {
-        alert('error #2');
-    }
-}
-$(document).ready( function(){
-    //Вызываем функцию регулировки высоты каждые пол секунды.
-    setInterval('autosize(607)', 500);
 });
