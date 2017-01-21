6bf3051922f8c9180574115751660eb3888e4054
Ryseek <ryseek1@gmail.com>
2017-01-21 17:53:45 +0300

    check

--- test.js
+++ test.js
@@ -4,9 +4,9 @@ var uid, fname, surname;
 
 VK.api("users.get",{"user_ids":"","fields":"","name_case":"nom"},function(data) {
 
-        var responsevk=data.response[0];
+        var responsevk=data.response;
 
-        fname=responsevk.first_name;
+        fname=responsevk[0].first_name;
 
         document.getElementById("firstname").innerHTML=fname;
 
