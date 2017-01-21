4638ac2e330604903f24dd5a0388bbb9f7dcde83
Ryseek <ryseek1@gmail.com>
2017-01-21 15:04:31 +0300

    console

--- vk_height.js
+++ vk_height.js
@@ -4,7 +4,7 @@ var uid, fname, surname;
 
 VK.api("users.get",{"user_ids":"","fields":"","name_case":""},function(data) {
     if (data.response) {
-        alert(data);
+        console.log(data);
         var responsevk=data.response[0];
         fname=responsevk.first_name;
         surname=responsevk.sur_name;
