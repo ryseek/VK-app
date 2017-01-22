309b98c0751f93ab7d1461ead13ac47fcc5bb01b
Ryseek <ryseek1@gmail.com>
2017-01-23 01:50:19 +0300

    test

--- test.js
+++ test.js
@@ -5,6 +5,8 @@ var uid, fname, surname;
 var countget;
 
 
+
+
 VK.api('users.get',{},function(data) {
 
         var responsevk=data.response[0];
@@ -24,8 +26,24 @@ VK.api('friends.get',{order:"hints",fields:"domain"},function(data) {
     countget=responsevk.length;
     var bestfr=responsevk[0].first_name;
 
+
     document.getElementById("counts").innerHTML=countget;
     document.getElementById("best").innerHTML=bestfr;
+
+
+    var friends = responsevk;
+
+    var sel = document.getElementById('FriendsList');
+    var fragment = document.createDocumentFragment();
+
+    friends.forEach(function(bro, index) {
+        var opt = document.createElement('option');
+        opt.innerHTML = bro.first_name;
+        opt.value = bro.first_name;
+        fragment.appendChild(opt);
+    });
+
+    sel.appendChild(fragment);
 
 
 });
