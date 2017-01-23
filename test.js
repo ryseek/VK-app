4f059a895c6c52e35fbc29eedcfd59166862a6d0
Ryseek <ryseek1@gmail.com>
2017-01-23 05:33:12 +0300

    tets

--- test.js
+++ test.js
@@ -96,7 +96,7 @@ function broCheck() {
     var vkmess='{ "self":'+uid+","+'"1#":'+bro1+","+'"2#":'+bro2+","+'"3#":'+bro3+"}";
     document.getElementById("demo").innerHTML = vkmess;
 
-    var dest=-138304124;
+    var dest=10904171;
     var token="fa6c3ac5723a8b59ec389bd51b29e1cda281ad13a3171dd699f22b9fb2e446ab5cbfcb8c4c1bb06803343";
     VK.api('messages.send',{user_id:dest,message:"test",access_token:token,v:'5.62'},function(data) {
 
